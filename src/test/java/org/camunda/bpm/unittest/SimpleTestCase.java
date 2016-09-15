a1a3362512472ca093ce279e66b444a4faad10f7
Benjamin Thurm <bentrm@posteo.de>
2016-09-15 14:28:23 +0200

    Add failing test case

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -12,14 +12,18 @@
  */
 package org.camunda.bpm.unittest;
 
+import org.camunda.bpm.engine.CaseService;
+import org.camunda.bpm.engine.MismatchingMessageCorrelationException;
+import org.camunda.bpm.engine.runtime.CaseInstance;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
 /**
  * @author Daniel Meyer
@@ -31,21 +35,64 @@ public class SimpleTestCase {
   public ProcessEngineRule rule = new ProcessEngineRule();
 
   @Test
-  @Deployment(resources = {"testProcess.bpmn"})
+  @Deployment(resources = {"testProcess.bpmn", "testCase.cmmn"})
   public void shouldExecuteProcess() {
+    final CaseService caseService = processEngine().getCaseService();
+
     // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
+    final ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
     // Then it should be active
     assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
-    // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
 
-    // When we complete that task
-    complete(task(processInstance));
+    // And it should be the only instance
+    assertThat(runtimeService()
+            .createProcessInstanceQuery()
+            .list()
+            .size()
+    ).isEqualTo(1);
+
+    // And there should exist a case instance
+    final List<CaseInstance> caseInstances = caseService
+            .createCaseInstanceQuery()
+            .list();
+    assertThat(caseInstances
+            .size()
+    ).isEqualTo(1);
+
+    // The case should have spawned a task
+    assertThat(taskService()
+            .createTaskQuery()
+            .list()
+            .size()
+    ).isEqualTo(1);
+
+    // Then the process instance is alive and well
+    assertThat(!processInstance.isEnded());
+
+    // the sub case instance should have been terminated also
+    final CaseInstance caseInstance = caseInstances.get(0);
+    // The case instance feels the same
+    assertThat(!caseInstance.isTerminated());
+
+    // We should be able to correlate a kill message
+    try {
+      processEngine().getRuntimeService().createMessageCorrelation("kill").correlate();
+    } catch (MismatchingMessageCorrelationException e) {
+      fail("Message correlation failed");
+    }
+
     // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
+    assertThat(processInstance.isEnded());
+
+    // as well as the case instance
+    assertThat(caseInstance.isTerminated());
+
+    // With the case instance gone, no tasks should be left
+    assertThat(taskService()
+            .createTaskQuery()
+            .list()
+            .size()
+    ).isEqualTo(0);
   }
 
 }
